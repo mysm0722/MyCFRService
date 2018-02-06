d7f21bd611ce7e3fa9e44c8d98a3fd85096e8181
changhwan <mysm0722@gmail.com>
2018-02-06 14:26:54 +0900

    init: init source

---
+++ demo/cfr_server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+//var client_id = '{YOUR_CLIENT_ID}';
+//var client_secret = '{YOUR_SECRET_KEY}';
+var client_id = 'TjPxrzlW6S5QKX940hev';
+var client_secret = 'KdSht6CYrC';
+var fs = require('fs');
+app.get('/face', function (req, res) {
+   var request = require('request');
+   //var api_url = 'https://openapi.naver.com/v1/vision/celebrity'; // 유명인 인식
+   var api_url = 'https://openapi.naver.com/v1/vision/face'; // 얼굴 감지
+
+   var _formData = {
+     image:'image',
+     image: fs.createReadStream('/usr/local/Cellar/nginx/1.12.2_1/html/mosaic/demo/han.jpg')
+     // FILE 이름
+   };
+    var _req = request.post({url:api_url, formData:_formData,
+      headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}}).on('response', function(response) {
+       console.log(response.statusCode) // 200
+       console.log(response.headers['content-type'])
+    });
+    console.log( request.head  );
+    _req.pipe(res); // 브라우저로 출력
+ });
+
+ app.listen(3000, function () {
+   console.log('http://127.0.0.1:3000/face app listening on port 3000!');
+ });
